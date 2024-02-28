b1f44feb3e7effd91c55d33f415ad07cfcc548d2
faldibule <faldibule@gmail.com>
2024-02-28 09:20:42 +0700

    First Commit

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -9,19 +9,19 @@ import { Box } from '@mui/material';
 const Page = forwardRef(({ children, title = '', meta, ...other }, ref) => (
   <>
     <Helmet>
-      <title>{`${title} | Terakom`}</title>
+      <title>{`${title} | Aquaelektronik`}</title>
       <meta name="description" content="undefined" />
 
       <meta property="og:url" content="https://github.com/theodevoid/v6-academy/blob/main/apps/web/src/components/HeadMetaData.tsx" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content={title + ' | Terakom'} />
+      <meta property="og:title" content={title + ' | Aquaelektronik'} />
       <meta property="og:description" content="undefined" />
       <meta property="og:image" content="" />
 
       <meta name="twitter:card" content="summary_large_image" />
       <meta property="twitter:domain" content="github.com" />
       <meta property="twitter:url" content="https://github.com/theodevoid/v6-academy/blob/main/apps/web/src/components/HeadMetaData.tsx" />
-      <meta name="twitter:title" content={title + ' | Terakom'} />
+      <meta name="twitter:title" content={title + ' | Aquaelektronik'} />
       <meta name="twitter:description" content="undefined" />
       <meta name="twitter:image" content="" />
       {meta}
